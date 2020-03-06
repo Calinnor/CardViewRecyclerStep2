6aece882ed4d8c7890ea0899a6f6c763c11c0fa8
Calinnor <frederic.hautson@gmail.com>
2020-03-06 19:04:24 +0100

    implement of share button action in toolbar, scaletype in cadrview

--- app/src/main/java/ocr/exercice/cardviewandrecyclerstep2/MainActivity.java
+++ app/src/main/java/ocr/exercice/cardviewandrecyclerstep2/MainActivity.java
@@ -6,10 +6,13 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.text.DateFormat;
@@ -22,49 +25,58 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdaptater;
     private RecyclerView.LayoutManager mLayoutManager;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ImageView covert_clickable_img = findViewById(R.id.covert_img1);
+
+        this.configureToolbar();
 
         Date actualDate = new Date();
         DateFormat dateFormat = DateFormat.getDateInstance();
 
         ArrayList<CardViewItem> cardViewList = new ArrayList<>();
-        cardViewList.add(new CardViewItem(R.drawable.paris_seconde_photo,"Line1","Ceci est le contenu des news en deux et blablabla","LX",dateFormat.format(actualDate)));
-        cardViewList.add(new CardViewItem(R.drawable.ppd,"Line3","Oula c'est pour tester les directives tout ca","LX",dateFormat.format(actualDate)));
-        cardViewList.add(new CardViewItem(R.drawable.paris_seconde_photo,"Line5","Un autre cast pour savoir si ca fonctionne","LX",dateFormat.format(actualDate)));
-        cardViewList.add(new CardViewItem(R.drawable.laaparis,"Line5","Ici le texte blablabla c'est comme ca pour pouvoir vérifier si il y a une ligne, deux ou beacoup plus. encore une fois :Ici le texte blablabla c'est comme ca pour pouvoir vérifier" +
-                "                  si il y a une ligne, deux ou beacoup plus\"","LX",dateFormat.format(actualDate)));
+        cardViewList.add(new CardViewItem(R.drawable.paris_seconde_photo, "Line1", "Ceci est le contenu des news en deux et blablabla", "LX", dateFormat.format(actualDate)));
+        cardViewList.add(new CardViewItem(R.drawable.ppd, "Line3", "Oula c'est pour tester les directives tout ca", "LX", dateFormat.format(actualDate)));
+        cardViewList.add(new CardViewItem(R.drawable.paris_seconde_photo, "Line5", "Un autre cast pour savoir si ca fonctionne", "LX", dateFormat.format(actualDate)));
+        cardViewList.add(new CardViewItem(R.drawable.laaparis, "Line5", "Ici le texte blablabla c'est comme ca pour pouvoir vérifier si il y a une ligne, deux ou beacoup plus. encore une fois :Ici le texte blablabla c'est comme ca pour pouvoir vérifier" +
+                "                  si il y a une ligne, deux ou beacoup plus\"", "LX", dateFormat.format(actualDate)));
 
-        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView = findViewById(R.id.recyclerViewId);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
         mAdaptater = new CardViewAdapter(cardViewList);
         mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdaptater); 
-        
+        mRecyclerView.setAdapter(mAdaptater);
+    }
+
+    private void configureToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.game_menu, menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.snap){
-            Toast.makeText(getApplicationContext(),"Pourquoi ca ne marche pas ???",Toast.LENGTH_SHORT);}
-        else if(id == R.id.blob){
-            Toast.makeText(getApplicationContext(),"Blob...lui il fonctionne. Mais euh.",Toast.LENGTH_SHORT);
+        switch (item.getItemId()) {
+            case R.id.share_button:
+                Toast.makeText(this, "Il ne faut pas oublier de mettre le .show apres le lengh toast sinon ca ne marche pas !!!" +
+                        " Deux heures de perdues à cause de ça...", Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return true;
     }
+
+
 }
